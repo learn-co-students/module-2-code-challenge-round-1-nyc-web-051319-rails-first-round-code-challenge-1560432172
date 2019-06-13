3640205db558ef1bd4cf308b129f09c85dc66f78
Aniket <aniketupatel1@gmail.com>
2019-06-13 10:58:15 -0400

    added a few comments and slight changes, still works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :powers
   resources :heroines
+
+  #below are the routes defined if I were to not use the resources method, I would not only have to create these routes but also helpers using (as: 'helper name')
+
+  #get '/heroines', to: 'heroines#index'
+  #get '/heroines/new', to 'heroines#new' as: 'new heroines_path'
+  #get '/heroines/:id', to: 'heroines#show'
+  #post '/heroines', to: 'heroines#create'
+
+  #get '/powers', to: 'powers#index'
+  #get 'powers/:id' to: 'powers#show'
 end
