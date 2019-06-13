555db2582ec2ddba35223795ed12025ce1764449
github username <github email address>
2019-06-13 11:09:20 -0400

    search bar

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,6 +1,10 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    if params[:search]
+      @heroines = Heroine.search_by_power(params[:search])
+    else
+      @heroines = Heroine.all
+    end
   end
 
   def show
@@ -25,6 +29,6 @@ class HeroinesController < ApplicationController
   private
 
   def heroine_params
-    params.require(:heroine ).permit(:name, :super_name, :power_id)
+    params.require(:heroine ).permit(:name, :super_name, :power_id, :search)
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,4 +1,11 @@
 class Heroine < ApplicationRecord
   belongs_to :power
   validates :super_name, uniqueness: true
+
+  def self.search_by_power(power)
+    self.all.select do |heroine|
+      heroine.power.name.downcase.include?(power.downcase)
+    end
+  end
+
 end
