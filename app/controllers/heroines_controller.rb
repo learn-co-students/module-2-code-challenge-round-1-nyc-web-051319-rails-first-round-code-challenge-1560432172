e84ede581d8ec89f2738c0db23692d45632b77c1
github username <github email address>
2019-06-13 10:04:34 -0400

    completed up to bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,49 @@
 class HeroinesController < ApplicationController
+  before_action :self_find, only: [:show, :edit, :update, :destroy]
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+    @power = @heroine.power
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    heroine = Heroine.create(heroine_params)
+    if heroine.valid?
+      heroine.save
+      redirect_to heroine_path(heroine)
+    else
+      flash[:message] = heroine.errors.full_messages[0]
+      redirect_to new_heroine_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
+
+  private
+
+  def self_find
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
