723fc681048b20e2714e296e6da156dbe6de56ac
Will Bainton <baintonw@gmail.com>
2019-06-13 10:29:07 -0400

    Basic challenge deliverables (hopefully)

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,38 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.create(power_params)
+    redirect_to power_path
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    @power.update(power_params)
+  end
+
+  def destroy
+    @power = Power.find(params[:id])
+    @power.delete
+    redirect_to powers_path
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :super_name, :power_id)
+  end
 end
