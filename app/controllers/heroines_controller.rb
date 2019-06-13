b79dbf2bae33dca48da765f1399443f361e1c9a3
chrisyuaners <chrisyuaners@gmail.com>
2019-06-13 10:17:28 -0400

    done. working on bonus.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,41 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show, :destroy]
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    heroine = Heroine.new(heroine_params)
+    if heroine.valid?
+      heroine.save
+      redirect_to heroine
+    else
+      flash[:message] = heroine.errors.full_messages[0]
+      redirect_to new_heroine_path
+    end
+  end
+
+  def destroy
+    @heroine.delete
+    redirect_to heroines_path
+  end
+
+  private
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
